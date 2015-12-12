459c971f3f698a6e96ea16d1cffad6e34c6275ad
BigBorg <770188954@qq.com>
2015-12-12 10:00:08 +0800

    ylab edition
    min to hour

--- MiPlot.R
+++ MiPlot.R
@@ -21,7 +21,7 @@ totalstep<-unname( unlist( lapply(stp,"[[","ttl") ) )
 png("total sleep duration.png")
 p<-ggplot(data.frame(date=MiData$date,sleepdur=sleepdur/60),aes(date,sleepdur))
 p<-p+geom_point()+geom_smooth()+geom_smooth(method="lm",color="red")
-p<-p+labs(title="Total Sleep")+ylab("Sleep Duration (min)")
+p<-p+labs(title="Total Sleep")+ylab("Sleep Duration (hour)")
 p
 dev.off()
 
@@ -29,7 +29,7 @@ dev.off()
 png("deep sleep duration.png")
 p<-ggplot(data.frame(date=MiData$date,deepsleep=deepsleep/60),aes(date,deepsleep))
 p<-p+geom_point()+geom_smooth()+geom_smooth(method="lm",color="red")
-p<-p+labs(title="Deep Sleep")+ylab("Deep Sleep Duration (min)")
+p<-p+labs(title="Deep Sleep")+ylab("Deep Sleep Duration (hour)")
 p
 dev.off()
 
