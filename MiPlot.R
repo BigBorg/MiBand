ef9f8b0b38700d6bcef922b7844bd8e52d432a0b
BigBorg <770188954@qq.com>
2016-01-21 15:45:27 +0800

    Fix problem extracting elements from list of list
    Fixing Problem

--- MiPlot.R
+++ MiPlot.R
@@ -13,9 +13,9 @@ MiData$date<-as.Date(MiData$date,"%Y-%m-%d")
 jsondata<-sapply(MiData$summary,fromJSON)
 slp<-jsondata[1,]
 stp<-jsondata[4,]
-sleepdur<-unname( unlist( lapply(slp,"[[","lt") )  )   ##extract elements from list of list
-deepsleep<-unname( unlist( lapply(slp,"[[","dp") )  )
-totalstep<-unname( unlist( lapply(stp,"[[","ttl") ) )
+sleepdur<-unname( unlist( lapply(slp,"[","lt") )  )   ##extract elements from list of list
+deepsleep<-unname( unlist( lapply(slp,"[","dp") )  )
+totalstep<-unname( unlist( lapply(stp,"[","ttl") ) )
 
 #plot 1
 png("total sleep duration.png")
